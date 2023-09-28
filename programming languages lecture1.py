1ecde34e96906d057259de530f8dd6bfed7a39fe
Sankysam <107345813+Sankysam@users.noreply.github.com>
2023-09-28 19:13:38 +0300

    Add files via upload
    first commit

---
+++ programming languages lecture1.py
@@ -0,0 +1,15 @@
+sadef greet_and_add():
+    # Ask for the user's name
+    name = input("Please enter your name: ")
+    print(f"Hello, {name}!")
+
+    # Ask for two numbers
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    # Add the numbers
+    result = num1 + num2
+    print(f"The sum of {num1} and {num2} is {result}.")
+
+# Call the function to run it
+greet_and_add()
